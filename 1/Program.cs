6e750ca179304f13084c601cd0160c03ea8d456d
PC-FLO\Florian <florian.ramasoft@hotmail.com>
2023-12-01 14:05:16 +0100

    Solve 1st puzzle

---
+++ 1/Program.cs
@@ -0,0 +1,8 @@
+﻿const string inputPath = @$".\Resources\input.txt";
+var sum = File.ReadAllLines(inputPath)
+    .Sum((line) => {
+        var firstDigit = line.FirstOrDefault(c => char.IsDigit(c)).ToString();
+        var lastDigit = line.LastOrDefault(c => char.IsDigit(c)).ToString();
+        return int.Parse(string.Concat(firstDigit, lastDigit));
+    });
+Console.WriteLine($"The sum is : {sum}");
